ab93b6492580f0faa4b10b6521beadc81f6ad9ba
Muhammad Haroon <140588538+haroonzaheer@users.noreply.github.com>
2023-10-27 10:58:14 -0700

    wepapi

---
+++ AspNetFirstWebApi/Controllers/EmployeesDataController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetFirstWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesDataController : ControllerBase
+    {
+        public string[] myEmployees = { "Haroon", "Adil", "Usama" };
+
+        [HttpGet]
+        public string[] GetEmployees()
+        {
+            return myEmployees;
+        }
+
+        [HttpGet("{id}")]
+        public string GetEmployeesById(int id)
+        {
+            return myEmployees[id];
+        }
+
+    }
+}
